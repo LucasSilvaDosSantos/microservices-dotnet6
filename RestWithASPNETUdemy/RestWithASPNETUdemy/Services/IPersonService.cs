24262e9e58337c95bcdad33800aa298fe43201b6
Lucas Santos <santos94us@hotmail.com>
2022-08-28 18:39:58 -0300

    Basic Verbs

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        IEnumerable<Person> FindAll();
+        Person Update(Person id);
+        void Delete(long id);
+    }
+}
---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonService.cs
@@ -0,0 +1,69 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementations
+{
+    public class PersonService : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public IEnumerable<Person> FindAll()
+        {
+            var people = new List<Person>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                var person = MockPerson(i);
+                people.Add(person);
+            }
+
+            return people;
+        }
+
+        public Person FindById(long id)
+        {
+            /*Mock*/
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name",
+                LastName = "Person LastName",
+                Address = "Person Address",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            /*ToDo retrieves the data that has been updated */
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Person Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+    }
+}
