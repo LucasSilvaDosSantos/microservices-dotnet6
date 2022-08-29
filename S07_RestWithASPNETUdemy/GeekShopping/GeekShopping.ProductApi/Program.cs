aab14dec85f5e658fbe70d537480a039b6768936
Lucas Santos <santos94us@hotmail.com>
2022-08-28 21:13:06 -0300

    add Product

--- S07_RestWithASPNETUdemy/GeekShopping/GeekShopping.ProductApi/Program.cs
+++ S07_RestWithASPNETUdemy/GeekShopping/GeekShopping.ProductApi/Program.cs
@@ -1,8 +1,17 @@
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//var connectionString = Configuration["MySQLConnection"];
+
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(builder.Configuration["MySQLConnection:MySqlConnectionString"], new MySqlServerVersion(new Version(8,0,30))));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
