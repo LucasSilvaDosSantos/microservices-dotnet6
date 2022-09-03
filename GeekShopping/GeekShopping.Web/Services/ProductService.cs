9a21a9ba104a18bfb756e437dce1390a7ddbda9c
Lucas Santos <santos94us@hotmail.com>
2022-09-03 00:43:27 -0300

    Consuming Product Microservice on Web Microservice

---
+++ GeekShopping/GeekShopping.Web/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using GeekShopping.Web.Util;
+
+namespace GeekShopping.Web.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _client;
+        public const string BasePath = "api/v1/product";
+
+        public ProductService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<ProductModel>> FindAll()
+        {
+            var response = await _client.GetAsync(BasePath);
+            return await response.ReadContentAs<List<ProductModel>>();
+        }
+
+        public async Task<ProductModel> FindById(long id)
+        {
+            var response = await _client.GetAsync($"{BasePath}/{id}");
+            return await response.ReadContentAs<ProductModel>();
+        }
+
+        public async Task<ProductModel> Create(ProductModel model)
+        {
+            var response = await _client.PostAsJsonAsync($"{BasePath}", model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProductModel>();
+
+            throw new Exception("Somenthing went wrong when calling API");
+        }
+
+        public async Task<ProductModel> Update(ProductModel model)
+        {
+            var response = await _client.PutAsJsonAsync($"{BasePath}", model);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<ProductModel>();
+
+            throw new Exception("Somenthing went wrong when calling API");
+        }
+
+
+        public async Task<bool> DeleteById(long id)
+        {
+            var response = await _client.DeleteAsync($"{BasePath}/{id}");
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<bool>();
+
+            throw new Exception("Somenthing went wrong when calling API");
+        }
+    }
+}
