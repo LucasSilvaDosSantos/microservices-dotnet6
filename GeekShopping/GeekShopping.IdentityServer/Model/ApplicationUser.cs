3c67b3b130f91da5cfc4f7c54b634a61b411b060
Lucas Santos <santos94us@hotmail.com>
2022-09-29 14:08:51 -0300

    Add DuendeSoftware

---
+++ GeekShopping/GeekShopping.IdentityServer/Model/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GeekShopping.IdentityServer.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        private string Firstname { get; set; }
+        private string Lastname { get; set; }
+    }
+}
